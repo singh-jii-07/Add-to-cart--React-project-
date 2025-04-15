92eaacc0ae3be2296fa1e51d1384a0a2848e42fb
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-04-15 16:55:38 -0500

    some more change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ import { AddContext } from "./Context/AddContext";
 
 const Navbar = () => {
   const {add}=useContext(AddContext);
-  const [Block , setBlock] = React.useState(false);
+  const [Show , setShow] = React.useState(true);
   const cardData = [
     {
       id: 1,
@@ -21,7 +21,7 @@ const Navbar = () => {
     },
   ];
   const Haldeldisplay=(()=>{
-   setBlock(!Block)
+   setShow(!Show)
   })
 
   return (
@@ -29,18 +29,21 @@ const Navbar = () => {
       <div className="text-2xl font-bold text-blue-600 cursor-pointer">
         MiniShop
       </div>
-      <ul className={`${Block?"flex":"hidden"} space-x-4`}>
+      {/* ${Show?"flex":"hidden"} */}
+      <ul className={`${Show?"flex":"hidden"}  space-x-4  `}>
         {cardData.map((card) => (
-          <li key={card.id} className="flex ">
+          <li key={card.id} className=" align-middle justify-center sm:hidden lg:block md:block">
             <Link
               to={card.link}
-              className="text-black hover:text-blue-600 transition font-semibold flex align-middle justify-center space-x-1"
+              className="text-black hover:text-blue-600 transition font-semibold align-middle justify-center space-x-1 flex "
             >
-             
+             <div className="flex">
+
               {card.name === "Cart" && <FaCartShopping className="text-lg mt-1 mr-1" />}
              
               {/* {card.name === "Product List" && <FaHome className="text-lg mt-1 mr-1" />} */}
               {card.name}
+             </div>
             </Link>
             {card.name === "Cart" && add.length > 0 && (
               <span className="text-black font-medium">
